84bb574fba587dfcc3cbc2b6946b854d746755c0
Sarfraz <sarfrazsiddiqi19@gmail.com>
2023-02-19 14:56:44 +0530

    implemented filter function, footer and oyher changes

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,8 +1,108 @@
-import React from 'react'
+import React,{ useState} from 'react'
+import './Footer.css'
+import {ReactComponent as Facebook} from '../assets/facebook.svg'
+import {ReactComponent as Twitter} from '../assets/twitter.svg'
+import {ReactComponent as Instagram} from '../assets/instagram.svg'
+import { FaHouzz} from "react-icons/fa"
+
 
 const Footer = () => {
+
+  const[email, setEmail] = useState("")
+
+  const handleChange = (e)=>{
+    setEmail(e.target.value)
+  }
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    fetch('https://property-hire-default-rtdb.asia-southeast1.firebasedatabase.app/emails.json',
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        emailId:email
+      })
+     }
+  )
+  .then(res =>res.json())
+  .then(data =>{
+    console.log('data:' , data)
+  })
+  setEmail('')
+  }
+
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-header'>
+        <div className='footer-header-item'>
+       <FaHouzz /> property for rent
+        </div>
+        <div className='footer-header-item'>
+       <FaHouzz /> property for rent
+        </div>
+        <div className='footer-header-item'>
+       <FaHouzz /> property for rent
+        </div>
+        <div className='footer-header-item'>
+       <FaHouzz /> property for rent
+        </div>
+      </div>
+      <div className='footer-container'>
+        <div className='footer-section footer-section-left col-sm-8'>
+          <div className='footer-section-column'>
+            <p className='footer-section-column-head'>ABOUT US</p>
+            <p>Our Story</p>
+            <p>Contact Us</p>
+            <p>Locate Stores</p>
+          </div>
+          <div className='footer-section-column'>
+            <p className='footer-section-column-head'>CUSTOMER CARE</p>
+            <p>Track Order</p>
+            <p>Shipping Policy</p>
+            <p>Terms and Conditions</p>
+            <p>Privacy Policy</p>
+            <p>FAQs</p>
+            <p>Terms of Service</p>
+            <p>Refund policy</p>
+          </div>
+          <div className='footer-section-column'>
+            <p className='footer-section-column-head'>OFFERS</p>
+            <p>Plant Parent Club</p>
+            <p>Ugaoo Coupons</p>
+          </div>
+        </div>
+        <div className='footer-section footer-section-right'>
+          <div className='footer-section-column'>
+            <p className='footer-section-column-head'>
+              SIGN UP FOR OUR NEWSLETTER   
+            </p>
+              For more property, houses  and our featured apartments <br></br> the week, exclusive offers and discounts
+
+            <p className='footer-section-column-head follow-us-container'>
+              FOLLOW US ON SOCIAL MEDIA
+            </p>
+            <div className='social-icons'>
+              <Facebook width='20px' />
+              <Twitter width='20px' />
+              <Instagram width='20px' />
+            </div>
+            <div className="email-section">
+            <h5 > Get In Touch</h5>
+              <form onSubmit={handleSubmit}> 
+            
+              <input type='text' placeholder="Your Email" value={email} onChange={handleChange} className="email-input"></input>
+              <button className="subscribe-btn">Subscribe</button>
+              
+              </form>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className='footer-bottom'>
+        Made with ❤️ by Sarfraz 
+      </div>
+
+    </div>
   )
 }
 
--- src/components/pages/Property.js
+++ src/components/pages/Property.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import { Col, Row, Select } from 'antd';
+import { Option } from 'antd/es/mentions';
+import React,{useState, useEffect} from 'react'
+import { Card } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './Property.css'
+
 
 const propertyList = [
   {
@@ -16,6 +21,7 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=400',
     location: 'Borivali',
     price: '25k',
+    amount: 25,
     size: '4bed',
   },
   {
@@ -23,7 +29,8 @@ const propertyList = [
     name: 'faulkner Ave',
     img: 'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=400',
     location: 'Bandra',
-    price: '30k',
+    price: '40k',
+    amount: 40,
     size: '3bed',
   },
   {
@@ -32,6 +39,7 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/209296/pexels-photo-209296.jpeg?auto=compress&cs=tinysrgb&w=400',
     location: 'Bandra',
     price: '35k',
+    amount: 35,
     size: '3bed',
   },
   {
@@ -40,7 +48,8 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/221024/pexels-photo-221024.jpeg?auto=compress&cs=tinysrgb&w=400',
     location: 'Church Gate',
     price: '30k',
-    size: '3bed',
+    amount: 30,
+    size: '2bed',
   },
   {
     id: 6,
@@ -48,7 +57,8 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/7601179/pexels-photo-7601179.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Mahim',
     price: '28k',
-    size: '3bed',
+    amount: 28,
+    size: '4bed',
   },
   {
     id: 7,
@@ -56,6 +66,7 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/3935320/pexels-photo-3935320.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Bandra',
     price: '20k',
+    amount: 20,
     size: '3bed',
   },
   {
@@ -63,7 +74,8 @@ const propertyList = [
     name: 'Magnolia Group',
     img: 'https://images.pexels.com/photos/7601155/pexels-photo-7601155.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Borivali',
-    price: '20k',
+    price: '30k',
+    amount: 30,
     size: '2bed',
   },
   {
@@ -72,7 +84,8 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/8186510/pexels-photo-8186510.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Navi Mumbai',
     price: '20k',
-    size: '3bed',
+    amount: 20,
+    size: '4bed',
   },
   {
     id: 10,
@@ -80,6 +93,7 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/13008560/pexels-photo-13008560.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Church Gate',
     price: '45k',
+    amount: 45,
     size: '3bed',
   },
   {
@@ -88,7 +102,8 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/816198/pexels-photo-816198.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Mahim',
     price: '20k',
-    size: '3bed',
+    amount: 20,
+    size: '2bed',
   },
   {
     id: 12,
@@ -96,16 +111,116 @@ const propertyList = [
     img: 'https://images.pexels.com/photos/2455398/pexels-photo-2455398.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     location: 'Church Gate',
     price: '20k',
+    amount: 20,
     size: '3bed',
   },
 ]
 const Property = () => {
+
+  const [data, setData]= useState([]);
+  const [location, setLocation] = useState("");
+  const [price, setPrice]=useState("");
+  const [bedSize,setBedSize] = useState("")
+
+  useEffect(() => {
+  setData(propertyList)
+  }, [])
+
+  const locationChange = (e)=>{
+
+    if(e!==undefined){
+      const newdata = propertyList.filter((item)=>{
+        return item.location == e
+     })
+     setData(newdata);
+    }else{
+      setData(propertyList)
+    }
+   
+  }
+
+  const priceChange = (e)=>{
+    // debugger
+    if(e!== undefined){
+      var lowPrice = parseInt(e.split('-')[0]);
+      var highPrice = parseInt(e.split('-')[1]);
+      
+  
+      var newPricedata = propertyList.filter((item)=>{
+        
+         return item.amount >=lowPrice && item.amount <highPrice;
+      })
+      setData(newPricedata);
+    }else{
+      setData(propertyList)
+    }
+    
+  }
+
+   const bedChange = (e)=>{
+    // debugger
+    if(e!== undefined){
+      const bedSize = propertyList.filter((item)=>{
+        return item.size == e
+     })
+     setData(bedSize);
+    }else{
+      setData(propertyList)
+    }
+    
+   }
+
   return (
-    <div className='listContainer'>
+
+    <div className='Container'>
+      <div className='filter_container'>
+        <h2>Search properties for rent</h2>
+        {/* <Card className='filter-items'> */}
+        <div >
+          <Row>
+            <Col span={4}>
+              <div>Location</div>
+              <div>
+                <Select style={{width:"90%"}} onChange={locationChange} allowClear>
+                  <Option key='Bandra'>Bandra</Option>
+                  <Option key='Church Gate'>Church Gate</Option>
+                  <Option key='Mahim'>Mahim</Option>
+                  <Option key='Borivali'>Borivali</Option>
+                  <Option key='Navi Mumbai'>Navi Mumbai</Option>
+                </Select>
+              </div>
+            </Col>
+            <Col span={4}>
+            <div>Price</div>
+              <div>
+                <Select style={{width:"90%"}} onChange={priceChange} allowClear>
+                  <Option key='20 - 30' value='20-30'>20k - 30k</Option>
+                  <Option key='30 - 40' value='30-40'>30k - 40k</Option>
+                  <Option key='40 - 50' value='40-50'>40k - 50k</Option>
+                 
+                </Select>
+              </div>
+            </Col>
+            <Col span={4}>
+            <div>Bed Size</div>
+              <div>
+                <Select style={{width:"90%"}} onChange={bedChange} allowClear>
+                  <Option key='2bed'>2bed</Option>
+                  <Option key='3bed'>3bed</Option>
+                  <Option key='4bed'>4bed</Option>
+                
+                </Select>
+              </div>
+            </Col>
+          </Row>
+        
+    </div>
+        {/* </Card> */}
+      </div>
       <p className='section-heading'></p>
   <div className='property-list'>
     {
-      propertyList.map((item)=>(
+      data.map((item)=>(
         <div className='property-list-item' key={item.id}>
           <div className="property-image-container">
           <img src={item.img} alt={item.name}></img>
